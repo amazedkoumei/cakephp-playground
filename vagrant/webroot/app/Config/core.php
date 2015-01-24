e321a2452108443895e331aa73e62719525935a1
amazedkoumei <amazed.koumei@gmail.com>
2015-01-24 22:19:58 +0900

    fix notice that is about Security.salt and Security.cipherSeed

--- vagrant/webroot/app/Config/core.php
+++ vagrant/webroot/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '3119caa6d1f5bcb22531f625e0306d9e8350fbea');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '844401573621704214632417983344');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
